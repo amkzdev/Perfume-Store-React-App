b93211041152ea6381b14fb0042beaa24d80810d
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-04-05 22:59:44 +0430

    Add Alias in Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import * as path from 'path'
 
-// https://vitejs.dev/config/
+
+const createAlis = (name: string) => ({ find: `@${name}`, replacement: path.resolve(__dirname, `src/${name}`) })
+const createRelativeAlias = (name: string) => ({ find: `@${name}`, replacement: path.resolve(__dirname, `src/${name}`) })
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      createAlis('components'),
+      createAlis('pages'),
+      createAlis('utils'),
+      createRelativeAlias('api'),
+      createRelativeAlias('app'),
+      createRelativeAlias('assets'),
+    ],
+  },
 })
