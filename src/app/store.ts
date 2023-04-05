7a525383a750738c2250a3544dae0db5d9ae589d
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-04-05 22:30:35 +0430

    Add Types To Redux

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import perfumeReducer from './features/perfume/perfumeSlice'
 export const store = configureStore({
     reducer: {
-
+        perfume: perfumeReducer
     }
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
