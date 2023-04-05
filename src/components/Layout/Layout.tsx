9958fc1453f52347edbdc9a0c4c0a3e21179ac03
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-04-05 23:22:34 +0430

    Add Style to Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import bg from '@assets/bg.svg'
 export const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div>
+        <div style={{ backgroundImage: `url(${bg})`, height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             You are in Layout
             {children}
         </div>
