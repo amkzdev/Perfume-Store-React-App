17e88223b22e50a454470cf7da9a534da28d9059
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-04-05 22:35:16 +0430

    Add Perfumes Route

--- src/route/PublicRoute/PublicRoute.tsx
+++ src/route/PublicRoute/PublicRoute.tsx
@@ -5,6 +5,7 @@ import {
     Route,
     Navigate
 } from "react-router-dom"
+import { Perfumes } from '../../pages/Perfumes/Perfumes'
 // import Dashboard from '../../components/dashboard/Dashboard'
 // import SignIn from '../../pages/Login/SignIn/SignIn'
 const PublicRoute = () => {
@@ -12,8 +13,8 @@ const PublicRoute = () => {
 
         <BrowserRouter>
             <Routes>
-                {/* <Route path="/login" element={<SignIn />} />
-                <Route path="/" element={<Navigate to='/login' />} />
+                <Route path="/perfumes" element={<Perfumes />} />
+                {/* <Route path="/" element={<Navigate to='/login' />} />
                 <Route path='*' element={<Navigate to='/login' />} /> */}
             </Routes>
         </BrowserRouter>
