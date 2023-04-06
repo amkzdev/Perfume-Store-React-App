e14fa99b2899ea5937931364acb2bab26dcc206b
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-04-06 13:35:31 +0430

    Init Perfumes

--- src/pages/Perfumes/Perfumes.tsx
+++ src/pages/Perfumes/Perfumes.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Grid } from '@mui/material'
+import { PerfumeListItem } from '@components'
+
+const p = [{}, {}, {}, {}, {}, {}]
 
 export const Perfumes = () => {
   return (
-    <div>Perfumes</div>
+    <Grid container direction={'column'} height={'100%'} spacing={2}>
+      {p.map(perfume => <PerfumeListItem {...perfume} />)}
+    </Grid>
   )
 }
