af4549177266dc763a350a2b0686daffd4af5359
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-04-06 09:32:21 +0430

    Add theme to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 import { AppRouter } from './route/AppRouter/AppRouter'
+import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material'
 
 function App() {
 
-  return <AppRouter />
+  return (<ThemeProvider theme={responsiveFontSizes(createTheme({}))}>
+    <AppRouter />
+  </ThemeProvider>)
 }
 
 export default App
