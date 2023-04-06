50b9e360166ac32e6648f1dcd3aab195dc6d6975
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-04-06 13:34:09 +0430

    Modify Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,8 +4,7 @@ import { Box } from '@mui/material'
 import { Footer } from './components'
 export const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <Box sx={{ backgroundImage: `url(${bg})`, height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative' }}>
-            You are in Layout
+        <Box sx={{ backgroundImage: `url(${bg})`, height: '100vh', display: 'flex',overflow:'auto', alignItems: 'center', justifyContent: 'center', position: 'relative' }}>
             {children}
             <Footer />
         </Box>
