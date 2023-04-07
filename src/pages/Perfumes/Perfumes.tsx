cda0db79f4cecb2ddf017d4806136ff9ebe90d11
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-04-07 23:28:16 +0430

    Add Max Width to Container

--- src/pages/Perfumes/Perfumes.tsx
+++ src/pages/Perfumes/Perfumes.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Grid } from '@mui/material'
 import { PerfumeListItem } from '@components'
 
-const p = [{}, {}, {}, {}, {}, {}]
+const p = [{}, {}, {}, {}, {}, {}, {}]
 
 export const Perfumes = () => {
   return (
-    <Grid container direction={'column'} height={'100%'} spacing={2}>
+    <Grid container direction={'column'} height={'100%'} spacing={4} sx={{ m: 1 }} maxWidth={'1024px'} overflow={'auto'}>
       {p.map(perfume => <PerfumeListItem {...perfume} />)}
     </Grid>
   )
