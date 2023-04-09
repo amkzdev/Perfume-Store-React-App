e38b8f5f14863593fb54eff03957a55e5f4bb6df
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-04-09 19:25:23 +0430

    Add Login To Route

--- src/route/PublicRoute/PublicRoute.tsx
+++ src/route/PublicRoute/PublicRoute.tsx
@@ -6,7 +6,7 @@ import {
     Navigate
 } from "react-router-dom"
 import { Perfumes } from '../../pages/Perfumes/Perfumes'
-import { SinglePerfume } from '@pages'
+import { Login, SinglePerfume } from '@pages'
 // import Dashboard from '../../components/dashboard/Dashboard'
 // import SignIn from '../../pages/Login/SignIn/SignIn'
 const PublicRoute = () => {
@@ -15,7 +15,8 @@ const PublicRoute = () => {
         <BrowserRouter>
             <Routes>
                 <Route path="/perfumes" element={<Perfumes />} />
-                <Route path="/perfume/:id" element={<SinglePerfume/>} />
+                <Route path="/perfume/:id" element={<SinglePerfume />} />
+                <Route path="/login" element={<Login />} />
                 {/* <Route path="/" element={<Navigate to='/login' />} />
                 <Route path='*' element={<Navigate to='/login' />} /> */}
             </Routes>
