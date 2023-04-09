4ce13250b915f15d26b049858d7d252641f804ce
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-04-09 19:21:15 +0430

    Add Style To Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,7 +4,7 @@ import { Box } from '@mui/material'
 import { Footer } from './components'
 export const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <Box sx={{ backgroundImage: `url(${bg})`, height: '100vh', display: 'flex',overflow:'auto', alignItems: 'center', justifyContent: 'center', position: 'relative' }}>
+        <Box sx={{ backgroundImage: `url(${bg})`, height: '100vh', display: 'flex', overflow: 'auto', alignItems: 'center', justifyContent: 'center', position: 'relative', ' & .MuiButton-root': { textTransform: 'unset !important' } }}>
             {children}
             <Footer />
         </Box>
