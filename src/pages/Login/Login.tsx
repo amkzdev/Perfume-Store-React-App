d1782b7c5543a8dccfabe5de89c9ed8af41730a1
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-04-09 19:31:18 +0430

    Add Button to Login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Box, Button, Typography } from '@mui/material'
 
 export const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <Box sx={{ display: 'flex', alignItems: 'center', backgroundColor: 'white', borderRadius: 199, border: '1px solid #707070' }}>
+
+
+            <Button sx={{ background: '#F0A500', color: '#000', width: 'fit-content', borderRadius: 48, px: 4 }} variant='contained'>
+                <Typography fontSize={80} color={'white'}>Login</Typography>
+            </Button>
+        </Box>
+    )
 }
